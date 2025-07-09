8eba705ecfb458cf5495cca0f5264fa0823caff7
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-09 09:32:26 +0300

    using JSX

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am a passionate and driven web developer with a creative mind and a love for turning ideas into clean, functional websites. Currently sharpening my skills in software engineering, I enjoy working with HTML, CSS, JavaScript, and React.</p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  )
 }
 
 export default About;
